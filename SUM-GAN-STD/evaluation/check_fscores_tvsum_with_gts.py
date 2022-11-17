e3dd265df77502e7dbdecfe98d88ae766030a522
Aria Minaidi <aria_2@live.com>
2022-11-17 19:52:38 +0200

    Update check_fscores_tvsum_with_gts.py

--- SUM-GAN-STD/evaluation/check_fscores_tvsum_with_gts.py
+++ SUM-GAN-STD/evaluation/check_fscores_tvsum_with_gts.py
@@ -5,11 +5,11 @@ import h5py
 from generate_summary import generate_summary
 from evaluation_metrics import evaluate_summary
 
-path = '/content/drive/MyDrive/SUM-GAN-STD/exp1/tvsum/results/split4' # path to the json files with the computed importance scores for each epoch
+path = '../SUM-GAN-STD/exp1/tvsum/results/split4' # path to the json files with the computed importance scores for each epoch
 results = listdir(path)
 results=results[:-1]
 results.sort(key=lambda video: int(video[6:-5]))
-PATH_TVSum = '/content/drive/MyDrive/Adversarial_Video_Summary-master/data/TVSum/eccv16_dataset_tvsum_google_pool5.h5'
+PATH_TVSum = '../data/TVSum/eccv16_dataset_tvsum_google_pool5.h5'
 eval_method = 'avg' # the proposed evaluation method for TVSum videos
 
 # for each epoch, read the results' file and compute the f_score
@@ -57,7 +57,7 @@ for epoch in results:
         all_f_scores.append(f_score)
 
     f_score_epochs.append(np.mean(all_f_scores))
-    print("f_score: ",np.mean(all_f_scores))
+    #print("f_score: ",np.mean(all_f_scores))
 
 with open(path+'/f_scores.txt', 'w') as outfile:  
     json.dump(f_score_epochs, outfile)   
